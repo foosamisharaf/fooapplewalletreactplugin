b277d11dc8d352505552de84b097649a6c49eef2
Sami Sharafeddine <sami.sharaf@foo-solutions.com>
2023-09-01 17:39:03 +0300

    added exports

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,43 @@ const Fooapplewalletreactplugin = NativeModules.Fooapplewalletreactplugin
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return Fooapplewalletreactplugin.multiply(a, b);
-}
+    export function setHostName(hostName: string , path:string): Promise<string> {
+      return Fooapplewalletreactplugin.setHostName(hostName , path);
+    }
+    export interface CardUserIdDetails {
+        userId?: string | null;  // nullable from "nullable NSString"
+        deviceId?: string | null; // nullable from "nullable NSString"
+        cardId: string;
+        cardHolderName: string;
+        cardPanSuffix: string;
+        sessionId?: string | null; // nullable from "nullable NSString"
+        localizedDescription?: string | null;
+     
+    
+    }
+    export interface CardPanIdDetails {
+        userId?: string | null;  // nullable from "nullable NSString"
+        deviceId?: string | null; // nullable from "nullable NSString"
+        cardId: string;
+        cardHolderName: string;
+        cardPanSuffix: string;
+        localizedDescription?: string | null; // nullable from "nullable NSString"
+        pan: string;
+        expiryDate: string;
+    
+    }
+    export function addCardForUserId({userId , deviceId , cardId , cardHolderName , cardPanSuffix , sessionId,localizedDescription }:CardUserIdDetails): Promise<string> {
+      return Fooapplewalletreactplugin.addCardForUserId(userId , deviceId , cardId , cardHolderName , cardPanSuffix , sessionId,localizedDescription);
+    }
+    export function addCardForPanId({userId , deviceId , cardId , cardHolderName , cardPanSuffix , localizedDescription , pan , expiryDate}:CardPanIdDetails): Promise<string> {
+      return Fooapplewalletreactplugin.addCardForUserIdWithPanAndExpiry(userId , deviceId , cardId , cardHolderName , cardPanSuffix , localizedDescription , pan , expiryDate);
+    }
+    export function deviceSupportsAppleWallet (): Promise<boolean> {
+      return Fooapplewalletreactplugin.deviceSupportsAppleWallet();
+    }
+    export function isCardAddedToLocalWalletWithCardSuffix (cardSuffix:string): Promise<boolean> {
+      return Fooapplewalletreactplugin.isCardAddedToLocalWalletWithCardSuffix(cardSuffix);
+    }
+    export function isCardAddedToRemoteWalletWithCardSuffix (cardSuffix:string): Promise<boolean> {
+      return Fooapplewalletreactplugin.isCardAddedToRemoteWalletWithCardSuffix(cardSuffix);
+    }
